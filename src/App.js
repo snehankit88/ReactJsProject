e09d627086fc285b065e600e1201688e3ee0c68a
snehankit88 <snehankit888@gmail.com>
2021-01-29 21:41:47 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -6,33 +6,70 @@ import faker from 'faker'
 function App() {
   const [name, setName] = useState('')
   const [showCard, setShowCard] = useState(true)
-  const buttonMarkup = (
-    <div>
-      <button className="button">YES</button>
-      <button className="button button3">NO</button>
-    </div>
-  )
-  const changeNameHandler = () => {
-    setName('snehankit Chikhalekar')
-  }
-  const changeInputHandler = event => setName(event.target.value)
+  const [cards,setCards]= useState([
+    {
+      id:'u1',
+      name:'snehankit',
+      avatar:'./images/img_avatar.png',
+      title:'Software Developer'
+    },
+    {
+      id:'u2',
+      name:'snehankit',
+      avatar:'./images/img_avatar.png',
+      title:'Software Developer'
+    },
+    {
+      id:'u3',
+      name:'snehankit1',
+      avatar:'./images/img_avatar.png',
+      title:'Software Developer'
+    }
+  ])
+
   const toggleShowCard = () => setShowCard(!showCard)
-  const cardsMarkup = showCard && <Card
-    avatar={faker.image.people()}
-    name={name}
-    title={faker.name.jobTitle()}
-    onChangeName={() => changeNameHandler('sneha Chikhalekar')}
-    onChangeInput={changeInputHandler}
-  >
-    {buttonMarkup}
-  </Card>
+
+  const deleteCardHandler = (cardIndex) => {
+    const cards_copy = [...cards]
+    cards_copy.splice(cardIndex,1)
+    console.log('cards_copy',cards_copy)
+    console.log('cards',cards)
+
+    setCards(cards_copy);
+  }
+
+const ChangeNameHandler = (event,id)=> {
+  //1 Which Card
+  const cardIndex = cards.findIndex(card=>card.id === id)
+
+  //2 make copy of card
+  const cards_copy = [...cards]
+
+  //3change name of specific card
+  cards_copy[cardIndex].name  = event.target.value
+
+  //4 set the cards with the latest version of card copy
+  setCards(cards_copy)
+
+}
+
+
+  const cardsMarkup = showCard && 
+  (
+    cards.map((card,index)=>   <Card
+      avatar={faker.image.people()}
+      name={card.name}
+      title={card.title}
+      key={card.id}
+      onDelete={()=> deleteCardHandler(index)}
+      onChangeName = {(event)=> ChangeNameHandler(event,card.id)}    
+    />)
+)
+ 
   return (
     <div className="App">
       <button className="button" onClick={toggleShowCard}>Hide card</button>
       {cardsMarkup}
-
-
-
     </div>
 
   );
--- src/Card.js
+++ src/Card.js
@@ -13,12 +13,11 @@ const Card = props => {
                 <div className="container">
                     <h4><b>{props.name}</b></h4>
                     <p>{props.title}</p>
-                    <input type="text" onChange={props.onChangeInput} />
-                    <p><button className="button" onClick={props.onChangeName}/></p>
+                    <input type="text" onChange={props.onChangeName} value={props.name}/> 
+                    <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
                     <p>{props.children}</p>
                 </div>
             </div>
-           
         </div>
     )
 }
